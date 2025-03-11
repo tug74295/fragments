2b4ca4fc8ecd8d6ec882688294fc6133a9f9555a
Kostandin Jorgji <tug74295@temple.edu>
2025-03-11 12:15:55 -0400

    Add spinner colors

--- app/src/main/java/edu/temple/fragments/ColorFragment.kt
+++ app/src/main/java/edu/temple/fragments/ColorFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class ColorFragment : Fragment() {
 
@@ -13,7 +15,12 @@ class ColorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color, container, false)
+        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+            findViewById<Spinner>(R.id.spinner).apply {
+                adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item,
+                    arrayOf("Blue", "Red", "White", "Black", "Magenta"))
+            }
+        }
     }
 
 }
